20a67771c971ae538bd2ed32737f4bb016549ecd
UzukiAme <aprilddev@gmail.com>
2015-09-15 05:36:52 -0400

    clean up center view

--- resources/views/layouts/center.blade.php
+++ resources/views/layouts/center.blade.php
@@ -13,26 +13,4 @@
     </div>
     @endif
   @endforeach
-  <!--<div id="skills-node" class="node">
-    <h1 class="skills-title title">Skills</h1>
-  </div>
-  <div id="projects-node" class="node">
-    <h1 class="projects-title title">Projects</h1>
-  </div>
-  <div id="center-node" class="node">
-    <h1 class="center-title">April Carter</h1>
-    <p>
-      Tweaking, tuning and building the web.
-    </p>
-  </div>
-  <div id="contact-node" class="node">
-    <h1 class="contact-title title">Contact</h1>
-  </div>
-  <div id="references-node" class="node">
-    <h1 class="references-title title">References</h1>
-  </div>
-  <div id="interests-node" class="node">
-    <h1 class="interests-title title">Interests</h1>
-  </div>
-</div>-->
 @endsection
